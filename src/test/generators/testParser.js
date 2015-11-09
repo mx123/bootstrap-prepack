5dfdea4912185e71d0110520ea5f52f3fd4cb056
Alexander Pustovalov <apustovalov@gmail.com>
2015-11-09 17:09:25 +0200

    generators #1 stage

--- src/test/generators/testParser.js
+++ src/test/generators/testParser.js
@@ -4,10 +4,10 @@ const data =
 'const { application,  application: { subApp1: { variable: variableName } } } = props;';
 
 const dataFunc =
-'(arg1, arg2) => { action(ref1, ref2); action(arg1, arg2); }';
+'(arg1, arg2) => { action($ref1, $ref2); action(arg1, arg2); }';
 
 try{
-    const ast = esprima.parse(dataFunc, {tolerant: true, range: false, comment: false});
+    const ast = esprima.parse(data, {tolerant: true, range: false, comment: false});
 
     console.log(JSON.stringify(ast, null, 4));
 } catch (e){
